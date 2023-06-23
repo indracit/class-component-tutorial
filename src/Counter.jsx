2c76d51d72468123990a9cd322a43e736216ed7d
Indracit S <indrajithij802@gmail.com>
2023-06-23 14:50:07 +0530

    final

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
+    constructor(props){
+        super(props)
+        this.state={
+            enable : false
+        }
+    }
+
+    static getDerivedStateFromProps(nextProps,nextState){
+
+        console.log(nextProps);
+        if(nextProps.state.counter === 0){
+
+            return {
+                enable : true
+            }
+        }
+        
+        return {
+            enable : false
+        }
+    }
+
     handleButtonClick = (num) => {
         this.props.handleClick(num);
     };
@@ -12,6 +34,7 @@ class Counter extends Component {
             <div className='post center'>
                 <h1>Counter</h1>
                 <p>{state.counter}</p>
+                {this.state.enable ? <h1>Dont press minus button</h1> : null}
                 <button onClick={()=>this.handleButtonClick(+1)}> + </button>
                 <button onClick={()=>this.handleButtonClick(-1)}> - </button>
             </div>
